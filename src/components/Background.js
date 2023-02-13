badb6cdb771621c4e2117cabba1ed537b7e95a58
febxx <ajifebri5@gmail.com>
2023-02-13 23:41:29 +0700

    update padding background

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Background = ({ children }) => {
 	return (
-		<div className="dark:bg-gray-900 min-h-screen transition-all lg:px-32 lg:p-12">
+		<div className="dark:bg-gray-900 min-h-screen transition-all lg:px-32 lg:p-12 px-6 p-4">
 			<div className="container mx-auto">
 					{children}
 			</div>
